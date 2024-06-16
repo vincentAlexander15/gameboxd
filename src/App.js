f8451627c95396c04a9ef87c1407484c522277ca
Esteban Charry <estebancharryy@gmail.com>
2024-06-16 15:01:11 -0700

    Trial run of getting data from placeholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          data: json,
+          isLoaded: true,
+        })
+      });
+  }
+
+  render () {
+
+    var { isLoaded, data } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div className="App">
+          <ul>
+            {data.map(datum => (
+              <li key={datum.id}>
+                Name: {datum.name} | Email: {datum.email}
+              </li>
+            ))};
+          </ul>
+        </div>
+      );
+    }
+      
+
+  }
+
 }
 
 export default App;
