fbec41e629c1587c0e937960dd6e0f94dd938d12
Vincent A <vincealex2012@gmail.com>
2024-07-03 16:03:05 -0700

    Implemented image carousel prototype for game page

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -2,24 +2,55 @@ import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Navbar from "../components/Navbar";
 import useFetch from '../components/useFetch';
+import '../styles/GamePage.css';
 
 const GamePage = () => {
+    //Retrieve data from DataPage
     const location = useLocation();
     const { gameData } = location.state || {};
-
+    
+    //Make API calls using useFetch
     const url = '/igdb/release_dates';
     const method = 'POST';
-    const body = `fields game, date, human; where game = ${gameData.id}; sort date asc; limit 10;`;
+    const body = `fields game, date, human; where game = ${gameData.id}; sort date asc; limit 1;`;
+    const scs = useFetch('/igdb/screenshots', method, `fields *; where game = ${gameData.id}; limit 10;`);
     const releaseDates = useFetch(url, method, body);
+
+    // //Cycles game screenshots for each page
+    const ScreenshotCarousel = ({scs}) => {
+        const [currentIdx, setCurrentIdx] = useState(0);
+
+        useEffect(() =>{
+            const updateIndex = () => {
+                setCurrentIdx(prevIndex => (prevIndex + 1) % scs.length);
+              };
+            const validIdx = setInterval(updateIndex, 3000);
+            return () => clearInterval(validIdx);
+        }, [scs]);
+
+        if (scs.length === 0) {
+            return (
+                <div>No Images Found</div>
+            )
+        }
+
+        return (
+            <div className="game_scs">
+                <img src={scs[currentIdx].url.replace('t_thumb', 't_1080p')}/>
+            </div>
+        );
+    };
 
     return (
         <div>
             <Navbar/>
-            <h1>{gameData ? gameData.name : "Loading..."}</h1>
-            <h1>
-                {/* grab oinly first date */}
-                {releaseDates && releaseDates[0].human}
-            </h1>
+            {scs ? <ScreenshotCarousel scs={scs} /> : <div>Loading screenshots...</div>}
+            <div class="container">
+                <h1>{gameData ? gameData.name : "Loading..."}</h1>
+                <h1>
+                    {releaseDates && releaseDates[0].human}
+                </h1>
+            </div>
         </div>
     );
 }
